0c7f353a9dfabc423746c81cbd09e153805da46d
pakhom-github <ypakhom20@gmail.com>
2024-03-15 12:42:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿while(true)
+    {
+
+    Console.Write(" Введите первое число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите второе число:  ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Что будем делать? +, -, /, *  ");
+    char action = Convert.ToChar(Console.ReadLine());
+
+    switch (action)
+    {
+        case '+':
+            int Resultplus = a + b;
+            Console.Write("Результат: " + Resultplus);
+            break;
+
+        case '-':
+            int Resultminus = a - b;
+            Console.Write("Результат: " + Resultminus );
+            break;
+
+        case '*':
+            int ResultMult = a * b;
+            Console.Write("Результат: " + ResultMult);
+            break;
+    
+        case '/':
+        int ResultFis = a / b;
+         Console.Write("Результат: " + ResultFis);
+            break;
+    }
+} 
